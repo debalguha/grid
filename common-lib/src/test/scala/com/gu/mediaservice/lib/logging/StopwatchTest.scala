6643f03f7f7af6952ca2d607fe6adf300a507915
Alex Ware <alex.ware@guardian.co.uk>
2020-03-16 15:09:57 +0000

    only have regular marker api here

--- common-lib/src/test/scala/com/gu/mediaservice/lib/logging/StopwatchTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/logging/StopwatchTest.scala
@@ -3,12 +3,21 @@ package com.gu.mediaservice.lib.logging
 import org.scalatest.{FunSpec, Matchers}
 
 class StopwatchTest extends FunSpec with Matchers {
-  it ("should return the elapsed time") {
+  it("should return the elapsed time") {
     val fiveSeconds: Long = 5 * 1000
+    val durationRegex = """\{duration=(\d*)\}""".r
+
     def doWork = Thread.sleep(fiveSeconds)
 
     val stopwatch = Stopwatch.start
     doWork
-    stopwatch.elapsed.duration.toMillis should be >= fiveSeconds // >= as time is needed to call the function
+    val elapsed = stopwatch.elapsed
+
+    (elapsed.toString match {
+      case durationRegex(duration) => {
+        duration.toLong
+      }
+      case _ => 0
+    }) should be >= fiveSeconds // >= as time is needed to call the function
   }
 }
